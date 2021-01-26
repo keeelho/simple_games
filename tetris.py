8a3bb1f08307847470dc9b6966cfb80f1c122da6
unknown <keeelho@gmail.com>
2021-01-26 19:39:55 +0900

    tetris init

---
+++ tetris.py
@@ -0,0 +1,16 @@
+import pygame
+import random
+
+pyagme.font.init()
+
+# Global vars
+s_width = 800
+s_height = 800
+play_width = 300
+play_height = 600
+block_size = 30
+
+top_left_x = (s_width - play_width) // 2
+top_left_y = s_height - play_height
+
+# shape formats

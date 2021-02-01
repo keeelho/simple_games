0ce7a30048506c1ba69d9630390ce664407179c1
unknown <keeelho@gmail.com>
2021-02-01 17:53:57 +0900

    pygame with network init

---
+++ online_game/client.py
@@ -0,0 +1,12 @@
+import pygame
+
+width = 500
+height = 500
+win = pygame.display.set_mode((width, height))
+pygame.display.set_caption("Client")
+
+clientNumber = 0
+
+def redrawWindow():
+    win.fill((255,255,255))
+    pygame.display.update()

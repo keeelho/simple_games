47d543b11d33c1f553c8603d9021642f892db53d
unknown <keeelho@gmail.com>
2021-02-09 18:17:41 +0900

    made rockpaperscissor game

--- online_game/client.py
+++ online_game/client.py
@@ -7,15 +7,6 @@ height = 500
 win = pygame.display.set_mode((width, height))
 pygame.display.set_caption("Client")
 
-clientNumber = 0
-
-def read_pos(str):
-    str = str.split(",")
-    return int(str[0], int(str[1]))
-
-def make_pos(tup):
-    return str(tup[0]) + "," + str(tup[1])
-
 def redrawWindow(win, player, player2):
     win.fill((255,255,255))
     player.draw(win)
@@ -30,12 +21,8 @@ def main():
 
     while run:
         clock.tick(60)
-        p2Pos = read_pox(n.send(make_pos((p.x,p.y))))
-        p2.x = p2Pos[0]
-        p2.y = p2Pos[1]
-        p2.update()
+        p2 = n.send(p)
 
-        clock.tick(60)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
